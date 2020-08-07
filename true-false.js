d27416ccbe81a7acc1afd334c861eac47ae05f78
sheikhhasib <hasib2130@gmail.com>
2020-08-07 10:25:15 +0600

    true and false value identify

---
+++ true-false.js
@@ -0,0 +1,22 @@
+/*
+true value 
+" "- white space is true value 
+'0'-string is true 
+[] - empty array is true 
+{} - empty object is true 
+
+false value 
+0 
+""
+null
+NaN
+undefined
+
+*/
+
+let name = {};
+if(name){
+    console.log("condition is true");
+}else{
+    console.log("condition is false");
+}
