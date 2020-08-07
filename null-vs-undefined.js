242c4a169d1145c3fb1115eafc79e1e1d6bb3e8e
sheikhhasib <hasib2130@gmail.com>
2020-08-07 11:57:08 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+// let pakhi ;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     // return num1+num2;
+// }
+
+
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+
+let premik = {
+    name : "hasib",
+    phone : '135464',
+}
+console.log(premik.gf);
